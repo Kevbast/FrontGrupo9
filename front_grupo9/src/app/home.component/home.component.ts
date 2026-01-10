55a3e126c4b297a11f14b4c851865d6027e144a1
marcospedroche05 <marcospedroche62@gmail.com>
2026-01-10 12:08:54 +0100

    Home terminado de diseñar, metodos para obtener eventos disponibles y ya transcurridos

--- front_grupo9/src/app/home.component/home.component.ts
+++ front_grupo9/src/app/home.component/home.component.ts
@@ -10,12 +10,34 @@ import { EventosService } from '../../services/eventosService';
 })
 export class HomeComponent implements OnInit {
   public eventos!: Array<Evento>
+  public eventosDisponibles!: Array<Evento>
+  public eventosTranscurridosMismoYear!: Array<Evento>
 
   constructor(private _service: EventosService){}
 
   ngOnInit(): void {
     this._service.getEventos().subscribe(result => {
       this.eventos = result;
+      this.filtrarEventosDisponibles();
+      this.filtrarEventosTranscurridosMismoYear();
     })
+  }
+
+  //METODO PARA OBTENER LOS EVENTOS DISPONIBLES PARA INSCRIBIRSE, ES DECIR, QUE SU FECHAEVENTO NO HAYA PASADO.
+  filtrarEventosDisponibles(): void {
+    const fechaActual = new Date();
+    this.eventosDisponibles = this.eventos.filter(evento => {
+      const fechaEvento = new Date(evento.fechaEvento);
+      return fechaEvento >= fechaActual;
+    });
+  }
+
+  //METODO PARA OBTENER LOS EVENTOS YA TRANSCURRIDOS EL MISMO AÑO
+  filtrarEventosTranscurridosMismoYear(): void {
+    const fechaActual = new Date();
+    this.eventosTranscurridosMismoYear = this.eventos.filter(evento => {
+      const fechaEvento = new Date(evento.fechaEvento);
+      return fechaEvento < fechaActual && fechaEvento.getFullYear() == fechaActual.getFullYear()
+    });
   }
 }
