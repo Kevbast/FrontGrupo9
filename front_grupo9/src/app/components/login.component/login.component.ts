37e3afc027f75ec5b69be361e5f09aaa741cf110
Kevin Bayas <kevbast936@gmail.com>
2026-01-11 10:54:00 +0100

    implemented methods: getrole and getroleid + loading gif on the login

--- front_grupo9/src/app/components/login.component/login.component.ts
+++ front_grupo9/src/app/components/login.component/login.component.ts
@@ -14,18 +14,46 @@ export class LoginComponent {
 @ViewChild('cajapassword') cajaPassword!: ElementRef;
 @Output()updateToken:EventEmitter<any>=new EventEmitter()
 
+// Variable para controlar la animación del botón (Cargando...)
+  public isLoading: boolean = false;
+  public errorMessage: string = "";
+
  constructor(private _service: ServiceTorneo, private _app: App, private _router: Router) { }
 
  login(): void{
     let usuario = this.cajaUsername.nativeElement.value;
     let password = this.cajaPassword.nativeElement.value;
 
-    this._service.login(usuario,password).subscribe(response=>{
-          localStorage.setItem('authToken', response.response);
-          console.log('Token JWT almacenado:', response.response);
-          this.updateToken.emit();
-          this._router.navigate(["/"])
-    })
+    if(!usuario || !password) {
+      this.errorMessage = "Por favor, rellena todos los campos";
+      return;
+    }
+
+    // this._service.login(usuario,password).subscribe(response=>{
+    //       localStorage.setItem('authToken', response.response);
+    //       console.log('Token JWT almacenado:', response.response);
+    //       console.log('Idrole almacenado:', response.idrole);
+    //       console.log('ROL almacenado:', response.role);
+    //       this.isLoading = false;         
+    //       this.updateToken.emit();
+    //       this._router.navigate(["/"])
+    // })
+    this._service.login(usuario, password).subscribe({
+      next: (response) => {
+        localStorage.setItem('authToken', response.response);
+        console.log('Login OK');       
+        // 2. DESACTIVAR CARGA Y REDIRIGIR
+        this.isLoading = false;         
+        this.updateToken.emit();
+        this._router.navigate(["/"]);
+      },
+      error: (error) => {
+        console.error(error);
+        this.errorMessage = "Credenciales incorrectas o error de servidor";
+        // 3. IMPORTANTE: DESACTIVAR CARGA EN ERROR
+        this.isLoading = false; 
+      }
+    });
   }
 
 }
--- front_grupo9/src/app/services/service.torneo.ts
+++ front_grupo9/src/app/services/service.torneo.ts
@@ -3,25 +3,57 @@ import { HttpClient,HttpHeaders } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { environment } from "../../environments/environment.development";
 
+import { tap } from "rxjs/operators"; // <--- Importante: Importar tap
+
+// Interfaz para saber qué nos devuelve la API exactamente
+export interface LoginResponse {
+  response: string; // El Token
+  role: string;
+  idrole: number;
+}
+
 @Injectable()
 export class ServiceTorneo{
      constructor(private _http:HttpClient){}
      //Login funcional del Torneo
-    login(nombre: string,contraseña:string): Observable<any> {
+    login(nombre: string,contraseña:string): Observable<LoginResponse> {
         let apiUrl=environment.apiTorneo+"api/auth/LoginEventos";
         let credentials={
             userName:nombre,
             password:contraseña
         }
         console.log(credentials);
-        return this._http.post<any>(apiUrl, credentials);   
-    }
+        return this._http.post<LoginResponse>(apiUrl, credentials).pipe(
+            tap((data: LoginResponse) => {
+                //Capturamos la respuesta
+                if (data.response) {
+                //Guardamos el token
+                localStorage.setItem('authToken', data.response);                
+                //Guardamos el Rol
+                localStorage.setItem('userRole', data.role);               
+                //Guardamos el ID del Rol (lo convertimos a string para guardarlo)
+                localStorage.setItem('userIdRole', data.idrole.toString());
+                }
+            })
+            );
+        };   
+    
     getToken(): string | null {
     return localStorage.getItem('authToken');
     }
 
+    getRole(): string | null {
+    return localStorage.getItem('userRole');
+    }
+
+    getIdRole(): string | null {
+        return localStorage.getItem('userIdRole');
+    }
+
     cerrarSesion(): void{
         localStorage.removeItem("authToken");
+        localStorage.removeItem("userRole");
+        localStorage.removeItem("userIdRole");
         this.getToken();
     }
 
